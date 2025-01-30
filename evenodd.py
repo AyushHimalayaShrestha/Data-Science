8a679247ce205316e446b0f44a7415dd74617cb5
Ayush  Shrestha <ayushshrestha@Ayushs-MacBook-Pro.local>
2025-01-30 11:52:34 +0545

    Data Science

---
+++ evenodd.py
@@ -0,0 +1,10 @@
+even=[]
+odd=[]
+for i in range(1,20):
+    if i%2==0:
+        even.append(i)
+    else:
+        odd.append(i)    
+else:
+    print(even)
+    print(odd)
---
+++ nestedif.py
@@ -0,0 +1,26 @@
+a=int(input("Enter first number : "))
+b=int(input("Enter second number : "))
+c=int(input("Enter third number : "))
+if a>b :
+    if a>c:
+     print("first number is greater")
+
+    else:
+       print("third number is greater")
+
+else :
+   if b>c:
+      print("second number is greater")
+   else:
+      print("third number is greater")
+
+
+
+character=input("Enter anyone character : ")
+if character in('a','e','i','o','u'):
+   print(f'{character} is vowel')     
+else:
+   print(f'{character} is not vowel')
+      
+
+
